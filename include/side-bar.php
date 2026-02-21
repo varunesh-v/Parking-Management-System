a3ae66b002ad18e177d6f3c70b6b9c9681cc6118
varunesh-v <varunesh1204@gmail.com>
2026-02-21 18:15:54 +0530

    Add files via upload

---
+++ include/side-bar.php
@@ -0,0 +1,31 @@
+<!-- Sidebar -->
+    <ul class="sidebar navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="dashboard.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="add-vehicle.php" style="color: white;">
+          <i class="fas fa-car-side"></i>
+          <span>Add Vehicle</span>
+        </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
+          <i class="fa fa-car"></i>
+          <span>Vehicle Category</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <a class="dropdown-item" href="add-category.php">Add Category</a>
+          <a class="dropdown-item" href="view-category.php">View Category</a>
+      </div>  
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="manage-vehicle.php" style="color: white;">
+          <i class="fa fa-list-alt"></i>
+          <span>Manage Vehicle</span>
+        </a>
+      </li>
+     </ul>
